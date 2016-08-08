70093a0d8f317ad9bac5df28aace129642cbc9f0
滕呈斌 <tengchengbin@192.168.0.101>
2016-08-08 09:53:03 +0800

    Initial Commit

---
+++ 宠汪/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  宠汪
+//
+//  Created by 滕呈斌 on 16/8/8.
+//  Copyright © 2016年 dapengpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
